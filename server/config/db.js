4d78dc999e75e9ca227ae5d44106e5920b3690cf
Saurav Kumar <skauumraarv2003@gmail.com>
2026-03-27 17:39:56 +0530

    fix: force hardcoded mongodb uri and ignore render env variables to resolve connection error

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // URL encoding the @ in the password to %40 prevent connection string parsing errors
-        const uri = process.env.MONGO_URI || 'mongodb+srv://sourav2002kumar:25Sau%402003@cluster0.jijh4wu.mongodb.net/jharkhand_tourism?retryWrites=true&w=majority';
+        // Force hardcoded URI in case the user accidentally set a bad MONGO_URI in Render dashboard
+        const uri = 'mongodb+srv://sourav2002kumar:25Sau%402003@cluster0.jijh4wu.mongodb.net/jharkhand_tourism?retryWrites=true&w=majority';
         const conn = await mongoose.connect(uri);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
